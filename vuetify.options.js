6d2874a97713138b14e7658e1def5d51d6aab1f7
Mohib Arshi <mohibarshi834@gmail.com>
2021-02-08 11:57:37 +0530

    init

---
+++ vuetify.options.js
@@ -0,0 +1,18 @@
+import colors from 'vuetify/es5/util/colors';
+
+export default {
+  theme: {
+    light: true,
+    themes: {
+      light: {
+        primary: '#F53838',
+        accent: colors.grey.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3
+      }
+    }
+  }
+}
+;
